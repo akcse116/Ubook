6c5f7e171d2ac2f89885fd5003586d8f95b90959
shijiazheng123 <szheng39@buffalo.edu>
2020-03-09 11:12:30 -0400

    testing of how to print the chat log in window on FRONTEND (backend implementation still basic)

--- message/views.py
+++ message/views.py
@@ -34,13 +34,19 @@ messages = [
     }
 ]
 
-# logs = {
-#     'person1':
-# }
+test = open('message/templates/message/person0:person2.txt', 'r').read()
+
+logs = {
+    ('person0', 'person2'): [i.split(": ", 1) for i in test.splitlines()]
+}
+
+user = 'person0'
 
 
 def home(request):
     context = {
-        'messages': messages
+        'messages': messages,
+        'log': logs[('person0', 'person2')],
+        'user': user
     }
     return render(request, 'message/chatlog.html', context)
