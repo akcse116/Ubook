85d61e9a27ab656451f7fdc4265f079e17be0d56
Alex Kogan <akogan@buffalo.edu>
2020-03-10 10:53:13 -0400

    reports dir

--- user_profile/views.py
+++ user_profile/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'templates/profile_draft.html')
+    return render(request, 'user_profile/profile_draft.html')
