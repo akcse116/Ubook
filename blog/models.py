3632fb1bf6ac857e20acf1ecdbbf18f48501aa0f
C. Holmes <cholmes0085@gmail.com>
2020-03-30 16:12:12 -0400

    created user model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# represent database structure as classes
+# classes are called models
+# django already has user model created that we will use
+
+
+class Post(models.Model):
+    # each attribute will be different field in DB
+
+    # max_length restraint
+    title = models.CharField(max_length=100)
+    # TextField similar to CharField but w/o restrictions
+    content = models.TextField()
+    # auto_now_add set date_posed to current date and time
+    # can't ever update - won't allow for edits
+    date_posted = models.DateTimeField(auto_now_add=True)
+    # pulls User from separate table created by django
+    # one-to-many relationship // one user can have multiple posts, but a post can only have 1 author
+    # ForeignKey(related table, what to do if user who created post gets deleted - delete post)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
