002590811730b21002a01c6c8424ab27a47972d0
Alex Kogan <akogan@buffalo.edu>
2020-04-07 22:53:02 -0400

    Signup post javascript expansion. Page quality changes

--- home/static/home/script.js
+++ home/static/home/script.js
@@ -1,17 +1,26 @@
 function post_signup() {
     let fname = document.getElementById("firstname");
-    console.log('First name is :' + fname.value);
+    console.log('First name is:' + fname.value);
     let lname = document.getElementById("lastname");
     let email = document.getElementById("email");
     let pass = document.getElementById("sign_password");
-    var	request	=	new	XMLHttpRequest();
-    request.onreadystatechange	=	function(){
-        if	(this.readyState	===	4	&&	this.status	===	200){
-            console.log(this.response);
-            //	Do	something	with	the	response
-        }
-    };
-    request.open("POST",	"/path");
-    let	data	=	{'first': fname, 'last': lname,	'email': email, 'password': pass}
-    request.send(JSON.stringify(data));
+    if(fname.value.length != 0 && lname.value.length != 0 && email.value.length != 0 && pass.value.length != 0){
+        fname.value = '';
+        lname.value = '';
+        email.value = '';
+        pass.value = '';
+        let	request	=	new	XMLHttpRequest();
+        request.onreadystatechange	=	function(){
+            if	(this.readyState	===	4	&&	this.status	===	200){
+                console.log(this.response);
+                //	Do	something	with	the	response
+            }
+        };
+        request.open("POST",	"/path");
+        let	data	=	{'first': fname.value, 'last': lname.value,	'email': email.value, 'password': pass.value}
+        request.send(JSON.stringify(data));
+    }
+    else{
+    }
+
 }
