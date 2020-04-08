c805c6f07c4a7a336f37d7c60b57c5b1c91829a1
Alex Kogan <akogan@buffalo.edu>
2020-04-07 23:26:23 -0400

    home JS, and "" to home reroute

--- home/static/home/script.js
+++ home/static/home/script.js
@@ -1,10 +1,14 @@
 function post_signup() {
+    let warn = document.getElementsByClassName('warning');
     let fname = document.getElementById("firstname");
     console.log('First name is:' + fname.value);
     let lname = document.getElementById("lastname");
     let email = document.getElementById("email");
     let pass = document.getElementById("sign_password");
     if(fname.value.length != 0 && lname.value.length != 0 && email.value.length != 0 && pass.value.length != 0){
+        if(warn[0].style.visibility === 'visible'){
+            warn[0].style.visibility = 'hidden';
+        }
         fname.value = '';
         lname.value = '';
         email.value = '';
@@ -14,13 +18,15 @@ function post_signup() {
             if	(this.readyState	===	4	&&	this.status	===	200){
                 console.log(this.response);
                 //	Do	something	with	the	response
+                /*if input deemed invalid inform user and reprompt else user is redirected*/
             }
         };
-        request.open("POST",	"/path");
-        let	data	=	{'first': fname.value, 'last': lname.value,	'email': email.value, 'password': pass.value}
+        request.open("POST",	window.location.host + "/signup");
+        let	data	=	{'first': fname.value, 'last': lname.value,	'email': email.value, 'password': pass.value};
         request.send(JSON.stringify(data));
     }
     else{
+        warn[0].style.visibility = 'visible';
     }
 
 }
