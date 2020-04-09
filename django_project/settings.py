d36b38f9e379cc8e82c1b754591ebfc8339c7cb6
C. Holmes <cholmes0085@gmail.com>
2020-04-09 17:47:17 -0400

    Merged all over to MySQL

--- django_project/settings.py
+++ django_project/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ubook_db',
+        'USER': 'admin',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '8000'
     }
 }
 
