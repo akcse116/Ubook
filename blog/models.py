0c399d598b06c148a47fb22a733205d98370b895
C. Holmes <cholmes0085@gmail.com>
2020-04-27 18:08:14 -0400

    Methods in Friend model add and remove friends

--- blog/models.py
+++ blog/models.py
@@ -45,4 +45,19 @@ class Post(models.Model):
 
 
 class Friend(models.Model):
-    users = models.ManyToManyField(User)
+    users = models.ManyToManyField(User)    #everyone else
+    current_user = models.ForeignKey(User, related_name='owner', null=True, on_delete=models.CASCADE)
+
+    @classmethod
+    def make_friend(cls, current_user, new_friend):
+        friend, created = cls.objects.get_or_create(
+            current_user=current_user
+        )
+        friend.users.add(new_friend)
+
+    @classmethod
+    def lose_friend(cls, current_user, new_friend):
+        friend, created = cls.objects.get_or_create(
+            current_user=current_user
+        )
+        friend.users.remove(new_friend)
