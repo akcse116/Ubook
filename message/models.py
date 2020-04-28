d60360aa3e8f7c776da1c08c107b88e8f26d1f9c
C. Holmes <cholmes0085@gmail.com>
2020-04-28 17:19:32 -0400

    Altered Message model to differentiate between author and recipient

--- message/models.py
+++ message/models.py
@@ -1,21 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import User
+from user_profile.models import User
 from django.conf import settings
 
-# Create your models here.
 
+class Message(models.Model):
+    # primary key identifier for Post object
+    id = models.AutoField(primary_key=True)
+    # TextField similar to CharField but w/o restrictions
+    content = models.TextField()
+    # can't ever update - won't allow for edits
+    date_posted = models.DateTimeField(auto_now_add=True)
+    # ForeignKey(related table, what to do if user who created post gets deleted - delete post)
+    author = models.ForeignKey(User, related_name="sender", on_delete=models.CASCADE, null=True, blank=True)
+    recipient = models.ForeignKey(User, related_name="receiver", on_delete=models.CASCADE, null=True, blank=True)
 
-# class Message(models.Model):
-#     # primary key identifier for Post object
-#     id = models.AutoField(primary_key=True)
-#     # TextField similar to CharField but w/o restrictions
-#     content = models.TextField()
-#     # can't ever update - won't allow for edits
-#     date_posted = models.DateTimeField(auto_now_add=True)
-#     # ForeignKey(related table, what to do if user who created post gets deleted - delete post)
-#     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
-#     # recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
-#
-#     # return how we want to be printed out
-#     def __str__(self):
-#         return self.content
+    # return how we want to be printed out
+    def __str__(self):
+        return self.content
